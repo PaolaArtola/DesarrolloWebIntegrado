3419b7f0417608cc4f4c9ce7a0fb472769abb7df
Paola Artola <130616755+PaolaArtola@users.noreplay.github.com>
2025-09-07 18:32:44 -0500

    Set up spring boot project and basic setup.

---
+++ backend/src/main/java/com/paolaartola/lacteos/LacteosApplication.java
@@ -0,0 +1,13 @@
+package com.paolaartola.lacteos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacteosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacteosApplication.class, args);
+	}
+
+}
