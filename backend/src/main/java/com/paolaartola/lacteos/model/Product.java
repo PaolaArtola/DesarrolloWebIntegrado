5c84966e97a1f96e885788e3e5de64939dda2979
Paola Artola <130616755+PaolaArtola@users.noreplay.github.com>
2025-09-07 20:55:57 -0500

    first model

---
+++ backend/src/main/java/com/paolaartola/lacteos/model/Product.java
@@ -0,0 +1,50 @@
+package com.paolaartola.lacteos.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
